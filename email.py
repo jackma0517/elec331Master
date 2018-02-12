c12099aa8a2cb81f9a439f4d73c3e419fa7681c7
ZHIYI JACK MA <jackma0517@gmail.com>
2018-02-12 11:43:32 -0800

    email done

--- email.py
+++ email.py
@@ -5,11 +5,14 @@ if __name__ == "__main__":
     endmsg = "\r\n.\r\n"
     # Choose a mail server (e.g. Google mail server) and call it mailserver
     gmailServer = 'smtp.gmail.com'
-    eceServer = 'mx1.ece.ubc.ca'
+    eceServer = 'esva.mail-relay.ubc.ca'
+    eceEmail = 'p3n0b@ece.ubc.ca'
+    gmail = 'jackma0517@gmail.com'
     mailPort = 25
     # Create socket called clientSocket and establish a TCP connection with mailserver
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((eceServer, mailPort))
+    mailServer = eceServer
+    clientSocket.connect((mailServer, mailPort))
 
     recv = clientSocket.recv(1024).decode()
     print(recv)
@@ -28,7 +31,10 @@ if __name__ == "__main__":
 
     # Send MAIL FROM command and print server response.
     print("FROM command")
-    fromAddress = '<p3n0b@ece.ubc.ca>'
+    if(mailServer == gmailServer):
+        fromAddress = gmail
+    else:
+        fromAddress = eceEmail
     fromCommand = 'MAIL FROM:' + fromAddress + "\r\n"
     clientSocket.send(fromCommand.encode())
     recv = clientSocket.recv(1024).decode()
@@ -38,7 +44,10 @@ if __name__ == "__main__":
 
     # Send RCPT TO command and print server response.
     print("RCPT command")
-    rcptAddress = '<jackma0517@gmail.com>'
+    if (mailServer == gmailServer):
+        rcptAddress = gmail
+    else:
+        rcptAddress = eceEmail
     rcptCommand = 'RCPT TO:' + rcptAddress + "\r\n"
     clientSocket.send(rcptCommand.encode())
     recv = clientSocket.recv(1024).decode()
